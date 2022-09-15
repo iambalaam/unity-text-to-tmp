9a968c4551dcddb97ee2b7a5e297c83c83773853
Guy Balaam <guy.balaam@unity3d.com>
2022-09-15 14:16:47 +0100

    Creating folder structure

---
+++ Assets/Plugins/Clean/Editor/ComponentProperties.cs
@@ -0,0 +1,25 @@
+namespace Plugins.Clean.Editor
+{
+    public static class ComponentProperties
+    {
+        public class Text
+        {
+            
+        }
+
+        public class TextMesh
+        {
+            
+        }
+
+        public class InputField
+        {
+            
+        }
+
+        public class Dropdown
+        {
+            
+        }
+    }
+}
