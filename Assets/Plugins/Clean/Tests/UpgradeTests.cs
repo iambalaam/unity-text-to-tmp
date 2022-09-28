9536b04796148c3475a20bc0523a68eaad273410
Guy Balaam <guy.balaam@unity3d.com>
2022-09-28 13:56:06 +0100

    Adding scene override test

--- Assets/Plugins/Clean/Tests/UpgradeTests.cs
+++ Assets/Plugins/Clean/Tests/UpgradeTests.cs
@@ -1,15 +1,20 @@
 using System;
 using System.IO;
 using NUnit.Framework;
+using NUnit.Framework.Internal;
 using Plugins.Clean.Editor;
 using TMPro;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
 namespace Plugins.Clean.Tests
 {
+    [TestFixture]
     public class UpgradeTests
     {
         private static Text CreateText(string text)
@@ -29,16 +34,25 @@ namespace Plugins.Clean.Tests
             if (prefab == null) throw new Exception("Could not return prefab");
 
             return prefab;
-
         }
 
-        private static string assetPath = "Assets/tmp";
-
-        [OneTimeSetUp]
-        public void CreateTmpDir()
+        public string Setup()
         {
-            if (!Directory.Exists(assetPath))
-                AssetDatabase.CreateFolder("Assets", "tmp");
+            var guid = Guid.NewGuid().ToString();
+            Debug.Log(guid);
+            if (!Directory.Exists($"Assets/{guid}")) ;
+            {
+                AssetDatabase.CreateFolder("Assets", guid);
+            }
+            return guid;
+        }
+
+        public void Cleanup(string guid)
+        {
+            if (Directory.Exists($"Assets/{guid}")) ;
+            FileUtil.DeleteFileOrDirectory($"Assets/{guid}");
+            if (File.Exists($"Assets/{guid}.meta"))
+                FileUtil.DeleteFileOrDirectory($"Assets/{guid}.meta");
         }
 
         [Test]
@@ -59,30 +73,69 @@ namespace Plugins.Clean.Tests
         [Test]
         public void CanUpgradeTextPrefab()
         {
-            var prefabPath = $"{assetPath}/text.prefab";
-            var prefab = CreateTextPrefab("original text", prefabPath);
-            var textInstance = Object.Instantiate(prefab);
-            var textComponent = textInstance.GetComponent<Text>();
+            var guid = Setup();
+            var prefabPath = $"Assets/{guid}/text.prefab";
 
-            var tmpComponent = Upgrade.UpgradeText(textComponent);
-            PrefabUtility.SaveAsPrefabAsset(tmpComponent.gameObject, prefabPath);
+            try
+            {
+                var prefab = CreateTextPrefab("original text", prefabPath);
+                var textInstance = Object.Instantiate(prefab);
+                var textComponent = textInstance.GetComponent<Text>();
 
-            var tmpInstance = Object.Instantiate(prefab);
-            var tmp = tmpInstance.GetComponent<TextMeshProUGUI>();
+                var tmpComponent = Upgrade.UpgradeText(textComponent);
+                PrefabUtility.SaveAsPrefabAsset(tmpComponent.gameObject, prefabPath);
 
-            Assert.IsNull(textInstance.GetComponent<Text>(), "Failed to remove Text component from prefab instance");
-            Assert.IsNotNull(textInstance.GetComponent<TextMeshProUGUI>(), "Failed to add TextMeshProUGUI component to existing prefab instance");
-            Assert.IsNotNull(tmp, "Failed to add TextMeshProUGUI component to new prefab instance");
-            Assert.AreEqual(tmp.text, "original text");
+                var tmpInstance = Object.Instantiate(prefab);
+                var tmp = tmpInstance.GetComponent<TextMeshProUGUI>();
+
+                Assert.IsNull(textInstance.GetComponent<Text>(),
+                    "Failed to remove Text component from prefab instance");
+                Assert.IsNotNull(textInstance.GetComponent<TextMeshProUGUI>(),
+                    "Failed to add TextMeshProUGUI component to existing prefab instance");
+                Assert.IsNotNull(tmp, "Failed to add TextMeshProUGUI component to new prefab instance");
+                Assert.AreEqual(tmp.text, "original text");
+            }
+            finally
+            {
+                Cleanup(guid);
+            }
+
+
+
         }
 
-        [OneTimeTearDown]
-        public void RemoveTmpDir()
+        [Test]
+        public void CanUpgradePrefabWithSceneOverrides()
         {
-            if (Directory.Exists(assetPath))
-                FileUtil.DeleteFileOrDirectory(assetPath);
-            if (File.Exists($"{assetPath}.meta"))
-                FileUtil.DeleteFileOrDirectory($"{assetPath}.meta");
+            var guid = Setup();
+            try
+            {
+                // Setup scene override
+                var prefab = CreateTextPrefab("original text", $"Assets/{guid}/text.prefab");
+                var scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+                var sceneInstance = Object.Instantiate(prefab);
+                var text = sceneInstance.GetComponent<Text>();
+                text.text = "scene override";
+                EditorSceneManager.SaveScene(scene, $"Assets/{guid}/test-scene.unity");
+
+                // Upgrade component
+                Upgrade.UpgradeText(text);
+                
+                // Check scene override
+                Assert.IsNull(sceneInstance.GetComponent<Text>(), "Failed to remove Text component from prefab instance");
+                Assert.IsNotNull(sceneInstance.GetComponent<TextMeshProUGUI>(), "Failed to add TextMeshProUGUI component from prefab instance");
+                Assert.AreEqual(sceneInstance.GetComponent<TextMeshProUGUI>().text, "scene override");
+
+                // Check prefab
+                var prefabInstance = Object.Instantiate(prefab);
+                Assert.IsNull(prefabInstance.GetComponent<Text>(), "Failed to remove Text component from prefab");
+                Assert.IsNotNull(prefabInstance.GetComponent<TextMeshProUGUI>(), "Failed to add TextMeshProUGUI component from prefab");
+                Assert.AreEqual(prefabInstance.GetComponent<TextMeshProUGUI>().text, "original text");
+            }
+            finally
+            {
+                Cleanup(guid);
+            }
         }
     }
 }
